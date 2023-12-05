b80a5db027b8c6360e1fec6ead8602a05ca80fbd
zMxVzi <84397113+zMxVzi@users.noreply.github.com>
2023-12-05 14:43:45 -0600

    Update config.py
    ruta de bd

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:sincontra1@localhost:3306/SIRET'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:sincontra1@db-siret.c1nv3t7xppqx.us-east-1.rds.amazonaws.com:3306/SIRET'
