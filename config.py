f1fdb923ac1379473fdb614218ae62e67dd213b8
mario <20203tn119@utez.edu.mx>
2023-12-07 03:12:20 -0600

    version 1.1

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config(object):
     SECRET_KEY = 'Clave'
 
 class DevelopmentConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:sincontra1@localhost:3306/SIRET'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:sincontra1@db-siret.c1nv3t7xppqx.us-east-1.rds.amazonaws.com:3306/SIRET'
